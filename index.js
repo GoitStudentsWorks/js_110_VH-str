09dc2e7de7a3e719bbc2fab61bf22ddaa2d3b616
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-12-02 12:32:42 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/js_110_VH-str@0ed14282e5f4aaf26cb4dda80f041cc08971f991 🚀

---
+++ index.js
@@ -0,0 +1,61 @@
+import{a as q,i as H,S as D,N as M,K as T}from"./assets/vendor-CmG41rRI.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))c(s);new MutationObserver(s=>{for(const o of s)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function r(s){const o={};return s.integrity&&(o.integrity=s.integrity),s.referrerPolicy&&(o.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?o.credentials="include":s.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function c(s){if(s.ep)return;s.ep=!0;const o=r(s);fetch(s.href,o)}})();const g=document.querySelector(".toggle"),j=document.body;g.addEventListener("click",()=>{j.classList.contains("bg-dark")?(j.classList.remove("bg-dark"),j.classList.add("bg-light"),g.classList.remove("dark")):(j.classList.remove("bg-light"),j.classList.add("bg-dark"),g.classList.add("dark"))});const E="/js_110_VH-str/assets/icons-yh5Ih9pv.svg",f="/js_110_VH-str/assets/projects-desk-03-min-bAztutHK.png",P="/js_110_VH-str/assets/projects-desk-03@2x-min-D7O6BJsL.png",B="/js_110_VH-str/assets/projects-mob-03-min-BsXsyoM6.png",$="/js_110_VH-str/assets/projects-mob-03@2x-min-tmcSzpo2.png",C="/js_110_VH-str/assets/projects-tabl-03-min-ZgDTbOh9.png",N="/js_110_VH-str/assets/projects-tabl-03@2x-min-Dqd8dn0C.png",h="/js_110_VH-str/assets/projects-desk-04-min-yxZx66BP.png",O="/js_110_VH-str/assets/projects-desk-04@2x-min-DHJNdR6B.png",z="/js_110_VH-str/assets/projects-mob-04-min-5yr3aGDi.png",I="/js_110_VH-str/assets/projects-mob-04@2x-min-BeXemoj4.png",A="/js_110_VH-str/assets/projects-tabl-04-min-Dt9XScdK.png",F="/js_110_VH-str/assets/projects-tabl-04@2x-min-B8Ml2TOY.png",v="/js_110_VH-str/assets/projects-desk-05-min-CfD1cLSZ.png",X="/js_110_VH-str/assets/projects-desk-05@2x-min-BRPn2X1J.png",K="/js_110_VH-str/assets/projects-mob-05-min-Db3py1Xm.png",R="/js_110_VH-str/assets/projects-mob-05@2x-min-B8zvmCB6.png",J="/js_110_VH-str/assets/projects-tabl-05-min-F3tbbktz.png",W="/js_110_VH-str/assets/projects-tabl-05@2x-min-BRqBFzgK.png",y="/js_110_VH-str/assets/projects-desk-06-min-X_gqtJm5.png",Z="/js_110_VH-str/assets/projects-desk-06@2x-min-Dj8eCfdF.png",Q="/js_110_VH-str/assets/projects-mob-06-min-C9QNW9Tj.png",U="/js_110_VH-str/assets/projects-mob-06@2x-min-DEtw7lg5.png",G="/js_110_VH-str/assets/projects-tabl-06-min-ugbcXMlD.png",Y="/js_110_VH-str/assets/projects-tabl-06@2x-min-CmmfjQzn.png",w="/js_110_VH-str/assets/projects-desk-07-min--lZv1p8n.png",ss="/js_110_VH-str/assets/projects-desk-07@2x-min-DmSkXiQs.png",es="/js_110_VH-str/assets/projects-mob-07-min-BSVWNsA-.png",ts="/js_110_VH-str/assets/projects-mob-07@2x-min-Dd3dUvIN.png",os="/js_110_VH-str/assets/projects-tabl-07-min-CPlUFmPk.png",rs="/js_110_VH-str/assets/projects-tabl-07@2x-min-BDIDMcu4.png",k="/js_110_VH-str/assets/projects-desk-08-min-xocuOah2.png",cs="/js_110_VH-str/assets/projects-desk-08@2x-min-BIWP6k6v.png",ns="/js_110_VH-str/assets/projects-mob-08-min-DBYeKoNQ.png",as="/js_110_VH-str/assets/projects-mob-08@2x-min-Ws78KLoX.png",is="/js_110_VH-str/assets/projects-tabl-08-min-C_fZqAdW.png",ls="/js_110_VH-str/assets/projects-tabl-08@2x-min-DRzjhCHA.png",L="/js_110_VH-str/assets/projects-desk-09-min-DsvIyQeP.png",ps="/js_110_VH-str/assets/projects-desk-09@2x-min-BvUdwZmo.png",ds="/js_110_VH-str/assets/projects-mob-09-min-DX2Z5Nr9.png",ms="/js_110_VH-str/assets/projects-mob-09@2x-min-Cc_85EFK.png",js="/js_110_VH-str/assets/projects-tabl-09-min-DT00A0X5.png",us="/js_110_VH-str/assets/projects-tabl-09@2x-min-BOveHSsW.png",u=[{alt:"Mimino website",img:f,sources:{mobile:{"1x":B,"2x":$},tablet:{"1x":C,"2x":N},desktop:{"1x":f,"2x":P}}},{alt:"Vyshyvanka vibes Landing Page",img:h,sources:{mobile:{"1x":z,"2x":I},tablet:{"1x":A,"2x":F},desktop:{"1x":h,"2x":O}}},{alt:"Power pulse webservice",img:v,sources:{mobile:{"1x":K,"2x":R},tablet:{"1x":J,"2x":W},desktop:{"1x":v,"2x":X}}},{alt:"Chego jewelry website",img:y,sources:{mobile:{"1x":Q,"2x":U},tablet:{"1x":G,"2x":Y},desktop:{"1x":y,"2x":Z}}},{alt:"Energy flow webservice ",img:w,sources:{mobile:{"1x":es,"2x":ts},tablet:{"1x":os,"2x":rs},desktop:{"1x":w,"2x":ss}}},{alt:"Starlight studio landing page",img:k,sources:{mobile:{"1x":ns,"2x":as},tablet:{"1x":is,"2x":ls},desktop:{"1x":k,"2x":cs}}},{alt:"Fruitbox online store",img:L,sources:{mobile:{"1x":ds,"2x":ms},tablet:{"1x":js,"2x":us},desktop:{"1x":L,"2x":ps}}}],gs=document.querySelector(".project-list"),V=document.querySelector(".more-projects");let bs=document.querySelector("li.project_card");V.addEventListener("click",xs);let d=0;function xs(){const e=bs.getBoundingClientRect().height,r=u.slice(d,d+3);gs.insertAdjacentHTML("beforeend",_s(r));let c=3;u.length-d<3&&(c=u.length-d),window.scrollBy({top:e*c,left:0,behavior:"smooth"}),d+=r.length,d>=u.length&&V.classList.add("hidden")}function _s(e){return e.map(({alt:t,img:r,sources:{mobile:c,tablet:s,desktop:o}})=>`
+      <li class="project_card">
+        <picture>
+          <source
+            media="(max-width: 767px)"
+              width="320"
+            height="194"
+            srcset="${c["1x"]} 1x, ${c["2x"]} 2x"
+          />
+          <source
+            media="(max-width: 1279px)"
+             width="704"
+            height="536"
+            srcset="${s["1x"]} 1x, ${s["2x"]} 2x"
+          />
+          <source
+            media="(min-width: 1280px)"
+             width="1008"
+            height="580"
+            srcset="${o["1x"]} 1x, ${o["2x"]} 2x"
+          />
+          <img
+            class="project-img-card"
+            src="${r}"
+            alt="${t}"
+            loading="lazy"
+          />
+        </picture>
+
+        <p class="project-skills">React, JavaScript, Node JS, Git</p>
+        <div class="bottom-box">
+          <h3 class="project-name">${t}</h3>
+          <a
+            class="visit-button"
+            href="https://github.com/VH-str/project-JS-TeamWork-FullStalkers"
+            target="_blank"
+          >
+            <span>VISIT</span>
+            <svg class="button_icon" width="24" height="24">
+              <use href="${E}#icon-arrow45"></use>
+            </svg>
+          </a>
+        </div>
+      </li>`).join("")}const b=document.querySelector(".ham-menu"),x=document.querySelector(".off-screen-menu");b.addEventListener("click",()=>{b.classList.toggle("active"),x.classList.toggle("active")});x.addEventListener("click",()=>{b.classList.toggle("active"),x.classList.toggle("active")});document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelectorAll(".faq-item"),t=e[0],r=e[0].querySelector(".faq-answer"),c=e[0].querySelector(".faq-arrow"),s=t.querySelector(".faq-question");t.classList.add("open"),r.classList.add("show"),c.classList.add("open"),s.classList.add("active"),e.forEach(n=>{const l=n.querySelector(".faq-question"),a=n.querySelector(".faq-answer"),p=n.querySelector(".faq-arrow");l.addEventListener("click",()=>{o(n,a,p,l)}),p.addEventListener("click",m=>{m.stopPropagation(),o(n,a,p,l)})});function o(n,l,a,p){const m=l.classList.contains("show");e.forEach(i=>{i.classList.remove("open"),i.querySelector(".faq-answer").classList.remove("show"),i.querySelector(".faq-arrow").classList.remove("open"),i.querySelector(".faq-question").classList.remove("active")}),m||(n.classList.add("open"),l.classList.add("show"),a.classList.add("open"),p.classList.add("active"))}});const S=document.querySelector(".swiper-wrapper"),fs=document.querySelector(".button-wrapper");function hs(e){const t=e.map(({author:r,avatar_url:c,review:s,_id:o})=>o===5?`<li class="swiper-slide">
+					<p class="reviews-text">${s.slice(0,-31)}</p>
+					<div class="card-wrapper">
+						<img class="reviews-icon" src="${c}" alt="${r}">
+						<h2 class="reviews-author">${r}</h2>
+					</div>
+				</li>`:`<li class="swiper-slide">
+					<p class="reviews-text">${s}</p>
+					<div class="card-wrapper">
+						<img class="reviews-icon" src="${c}" alt="${r}">
+						<h2 class="reviews-author">${r}</h2>
+					</div>
+				</li>`).join("");S.innerHTML=t,fs.classList.remove("visually-hidden")}function vs(){const e=`<li class="not-found swiper-slide">
+	<p class="not-found-text">Not found!</p>
+</li><li class="not-found swiper-slide">
+	<p class="not-found-text">Not found!</p>
+</li>`;S.innerHTML=e}async function ys(e){try{const{data:t}=await q.get("https://portfolio-js.b.goit.study/api/reviews");hs(t)}catch(t){vs(),H.error({position:"topRight",message:t.message})}}ys();new D(".swiper",{modules:[M,T],direction:"horizontal",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},keyboard:{enabled:!0},slidesPerView:1,spaceBetween:32,breakpoints:{1280:{slidesPerView:2}}});document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector(".input-info"),t=document.querySelector(".backdrop"),r=document.querySelector(".modal-message"),c=document.querySelector(".modal-close-btn"),s="https://portfolio-js.b.goit.study/api/requests";function o(a){r.innerHTML=`<h3 class="modal-title">${a.title}</h3><p class="modal-text">${a.message}</p>`,t.classList.remove("is-hidden-back"),document.body.style.overflow="hidden"}function n(){t.classList.add("is-hidden-back"),document.body.style.overflow=""}function l(a){a.key==="Escape"&&n()}t.addEventListener("click",a=>{a.target===t&&n()}),c.addEventListener("click",n),e.addEventListener("submit",async a=>{a.preventDefault();const p=e.querySelector("#try-agen").value.trim(),m=e.querySelector("#client_comment").value.trim();try{const i=await fetch(s,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({email:p,comment:m})});if(!i.ok)throw new Error("Failed to send the request. Please try again.");const _=await i.json();o(_),e.reset(_)}catch(i){H.error({title:"Error",message:i.message||"Something went wrong. Please try again.",position:"center"})}}),document.addEventListener("keydown",l)});
+//# sourceMappingURL=index.js.map
